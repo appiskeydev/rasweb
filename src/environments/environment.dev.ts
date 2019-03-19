f9daef7a902ea684ba614b21863cda6e3e027167
Suraksha-Sahani <payalsahani77@gmail.com>
2019-03-19 19:00:13 +0500

    Keycloak in progress

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,7 +1,7 @@
 export const environment = {
     // apiUrl: 'http://localhost:8080/api/raservice/v1',
 
-    apiUrl: 'http://localhost:8080/api/raservice/v1',
+    apiUrl: 'http://rasdev.scaleablecloud.com/api/raservice/v1',
 
     production:false,
     dev: true,
