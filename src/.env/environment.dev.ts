d536ce53a904fea727a6b4a9a8378bc06a084846
Suraksha-Sahani <payalsahani77@gmail.com>
2019-03-26 14:13:07 +0500

    gitignore changed

---
+++ src/.env/environment.dev.ts
@@ -0,0 +1,15 @@
+
+
+
+
+
+export const environment = {
+
+    apiUrl: 'http://rasdev.scaleablecloud.com/api/raservice/v1',
+
+    production:false,
+    dev: true,
+    hmr       : false,
+    secure : false
+
+};
---
+++ src/.env/environment.hmr.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: false,
+    hmr       : true,
+    dev:false,
+    apiUrl: 'http://localhost:8080/api/raservice/v1'
+    // apiUrl: 'http://ras.scaleablecloud.com/api/raservice/v1'
+
+
+
+};
