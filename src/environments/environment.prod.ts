1c8e972c46272be1f76e6ed265902c0bb0664f89
Suraksha-Sahani <payalsahani77@gmail.com>
2019-03-26 18:12:33 +0500

    gitignore changed with .env variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,12 +3,5 @@ export const environment = {
     production: true,
     hmr: false,
     dev: false,
-    apiUrl: 'http://rasprod.scaleablecloud.com/api/raservice/v1',
-
-    // assets: {
-    //     dotaImages:
-    //         'https://cdn-keycloak-angular.herokuapp.com/assets/images/dota-heroes/'
-    // },
-    // apis: { dota: 'http://localhost:3000' },
-    // keycloak: keycloakConfig
+    apiUrl: 'http://rasprod.scaleablecloud.com/api/raservice/v1'
 };
