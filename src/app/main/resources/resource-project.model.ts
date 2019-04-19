562134a4af537244cfdccdfd5c243cd5a3fa7d9c
Suraksha-Sahani <payalsahani77@gmail.com>
2019-04-19 14:18:27 +0500

    api integration in progress

--- src/app/main/resources/resource-project.model.ts
+++ src/app/main/resources/resource-project.model.ts
@@ -1,13 +1,44 @@
 import { Project } from "../projects/project.model";
+import { Resource } from "./resource.model";
+import { FuseUtils } from "@fuse/utils";
 
 
 export class ResourceProject {
 
+    id:string;
+    name: string;
+    handle:string;
+    updatedAt: string;
+    createdAt: string;
     project: Project;
-
+    resource:Resource;
     resourceProjectHour: string;
     resourceProjectAllocation: string;
     resourceProjectWorkStartDate: string;
     resourceProjectWorkEndDate: string;
 
+    /**
+        * Constructor
+        *
+        * @param Resource
+        */
+    constructor(resourceProject?) {
+
+        resourceProject = resourceProject || {};
+        if (resourceProject.name !== '') {
+            this.handle = FuseUtils.handleize(resourceProject.name + '');
+        }
+        this.id = resourceProject.id || '';
+        this.name = resourceProject.name || '';
+        this.updatedAt = resourceProject.updatedAt || '';
+        this.createdAt = resourceProject.createdAt || '';
+        this.project = resourceProject.project || null;
+        this.resource = resourceProject.resource || null;
+        this.resourceProjectHour = resourceProject.resourceProjectHour || '';
+        this.resourceProjectAllocation = resourceProject.resourceProjectAllocation || '';
+        this.resourceProjectWorkStartDate = resourceProject.resourceProjectWorkStartDate || '';
+        this.resourceProjectWorkEndDate = resourceProject.resourceProjectWorkEndDate || '';
+    }
 }
+
+
