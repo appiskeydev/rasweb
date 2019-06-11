61be96e46d850a8abe877c8a2eb545d30ba59ffb
Tahir Saleem <tahir.saleem@appiskey.com>
2019-06-11 20:32:52 +0500

    resolve dependancy issue and import packages

--- src/app/main/payments/payments.module.ts
+++ src/app/main/payments/payments.module.ts
@@ -1,11 +1,45 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { PaymentDailogFormComponent } from './payment-dailog-form/payment-dailog-form.component';
 import { PaymentListProjectComponent } from './payment-list-project/payment-list-project.component';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule, MatChipsModule, MatExpansionModule, MatFormFieldModule, MatIconModule, MatInputModule, MatPaginatorModule, MatRippleModule, MatSelectModule, MatSortModule, MatSnackBarModule, MatTableModule, MatTabsModule, MatAutocompleteModule, MatMenuModule, MatDialogModule, MatCheckboxModule, MatToolbarRow, MatToolbar, MatDatepicker, MatDatepickerToggle, MatToolbarModule, MatDatepickerModule, MatCardModule, MatSliderModule, MatSlideToggleModule } from '@angular/material';
+import { FuseSharedModule } from '@fuse/shared.module';
+import { FuseConfirmDialogModule, FuseSidebarModule } from '@fuse/components';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+
+    MatChipsModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSortModule,
+    MatSnackBarModule,
+    MatTableModule,
+    MatTabsModule,
+    MatAutocompleteModule,
+    MatMenuModule,
+    MatDialogModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatDialogModule,
+    MatCardModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+
+
+    MatButtonModule,
+
+    FuseSharedModule,
+    FuseConfirmDialogModule,
+    FuseSidebarModule,
   ],
   declarations: [PaymentDailogFormComponent, PaymentListProjectComponent]
 })
